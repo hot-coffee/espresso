d836d8b08c1043e0c29da8cce741425200523020
Andre Green <acgreen1226@gmail.com>
2015-10-28 22:46:56 -0700

    added option to prepopulate database. fetch/insert clients

--- campfire/js/db-handler.js
+++ campfire/js/db-handler.js
@@ -1,15 +1,14 @@
 'use strict';
 
-var Db = require('mongodb').Db;
 var MongoClient = require('mongodb').MongoClient;
-var Logger = require('./Logger');
+var Logger = require('./logger');
+var ObjectID = require('mongodb').ObjectID;
 
 function DbHandler() {
     this.dbPath = 'mongodb://localhost:27017/campfire';
 
-    this.connect = function(callback) {
+    this._connect = function(callback) {
         var self = this;
-
         console.log('url:', this.dbPath);
         MongoClient.connect(this.dbPath, function (err, db) {
             if (err) {
@@ -27,17 +26,13 @@ function DbHandler() {
 
     this.insert = function(collectionName, insertObjects, callback) {
         Logger.log('Inserting objects: ' + JSON.stringify(insertObjects), __filename, false, false);
-        var self = this;
-        this.connect(function (err, db) {
-            if (err) {
-                Logger.log('Error: failed to connect to db: ' + self.dbPath, __filename, true, false);
-                throw err;
-            }
+        this._connect(function (err, db) {
+            this._handleConnectionError(err);
 
-            Logger.log('Connected to database: ' + self.dbPath, __filename, false, false);
+            Logger.log('Connected to database: ' + this.dbPath, __filename, false, false);
 
             var collection = db.collection(collectionName);
-            collection.insertMany(insertObjects, function(error, result) {
+            collection.insertMany(insertObjects, function (error, result) {
                 db.close();
                 if (error) {
                     Logger.log(
@@ -63,7 +58,71 @@ function DbHandler() {
                     callback(result);
                 }
             });
-        });
+        }.bind(this));
+    };
+
+    this.retrieveAll = function (collectionName, callback) {
+        Logger.log('Retrieving objects for: ' + collectionName, __filename, false, false);
+        this._connect(function (err, db) {
+            this._handleConnectionError(err);
+
+            Logger.log('Connected to database: ' + this.dbPath, __filename, false, false);
+
+            var collection = db.collection(collectionName);
+            collection.find().toArray(function(error, result) {
+                db.close();
+                if (error) {
+                    Logger.log(
+                        'Error: retrieving all objects from: ' + collectionName,
+                        __filename,
+                        true,
+                        false
+                    );
+
+                    throw error;
+                }
+
+                if (callback) {
+                    callback(result);
+                }
+            });
+        }.bind(this));
+    };
+
+    this.retrieveWithId = function (collectionName, id, callback) {
+        Logger.log('Retrieving objects for: ' + collectionName, __filename, false, false);
+        this._connect(function (err, db) {
+            this._handleConnectionError(err);
+
+            Logger.log('Connected to database: ' + this.dbPath, __filename, false, false);
+
+            var objectId = new ObjectID(id);
+            var collection = db.collection(collectionName);
+            collection.find({_id:objectId}).toArray(function(error, result) {
+                db.close();
+                if (error) {
+                    Logger.log(
+                        'Error: retrieving all objects from: ' + collectionName,
+                        __filename,
+                        true,
+                        false
+                    );
+
+                    throw error;
+                }
+
+                if (callback) {
+                    callback(result);
+                }
+            });
+        }.bind(this));
+    };
+
+    this._handleConnectionError = function(err) {
+        if (err) {
+            Logger.log('Error: failed to connect to db: ' + this.dbPath, __filename, true, false);
+            throw err;
+        }
     };
 }
 
--- campfire/js/request-handler.js
+++ campfire/js/request-handler.js
@@ -1,20 +1,33 @@
 'use strict';
 
 var DbHandler = require('./db-handler');
-var Logger = require('./Logger');
+var Logger = require('./logger');
 
 function RequestHandler() {
-    this.dbHandler = new DbHandler();
-
     this.insertClient = function(req, res) {
         Logger.log('Headers:' + JSON.stringify(req.headers), __filename, false, false);
         Logger.log('Incoming data: '+ req.body, __filename, false, false);
         var data = req.body;
         var dbHandler = new DbHandler();
-        dbHandler.insert('clients', [data], function(result) {
+        dbHandler.insert(this.collections.clients, [data], function(result) {
             var message = !!result ? "It's Steven's world!!" :
                 "Dude, couldn't insert that object...Bummer";
             res.send(JSON.stringify(message));
+        });
+    };
+
+    this.getAllClients = function (req, res) {
+        var dbHandler = new DbHandler();
+        dbHandler.retrieveAll('clients', function (result) {
+            res.send(JSON.stringify(result));
+        });
+    };
+
+    this.getClient = function (req, res) {
+        var dbHandler = new DbHandler();
+        var id = req.params.clientId;
+        dbHandler.retrieveWithId('clients', id, function (result) {
+            res.send(JSON.stringify(result));
         });
     };
 }
