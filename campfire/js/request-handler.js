53a4f7a283d437d28b03c73538ca3e5b0d99d128
greenac <andre1226.dev@gmail.com>
2015-11-04 11:30:41 -0800

    Fixed error in request handler

--- campfire/js/request-handler.js
+++ campfire/js/request-handler.js
@@ -6,10 +6,10 @@ var Logger = require('./logger');
 function RequestHandler() {
     this.insertClient = function(req, res) {
         Logger.log('Headers:' + JSON.stringify(req.headers), __filename, false, false);
-        Logger.log('Incoming data: '+ req.body, __filename, false, false);
+        Logger.log('Incoming data: '+ JSON.stringify(req.body), __filename, false, false);
         var data = req.body;
         var dbHandler = new DbHandler();
-        dbHandler.insert(this.collections.clients, [data], function(result) {
+        dbHandler.insert('clients', [data], function(result) {
             var message = !!result ? "It's Steven's world!!" :
                 "Dude, couldn't insert that object...Bummer";
             res.send(JSON.stringify(message));
